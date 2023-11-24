f90e26eca5e2021a56fe15df39160b600ef31b6d
SkrinGod <Rooopppi1@gmail.com>
2023-11-24 14:20:41 +0300

    Homework 1

---
+++ Homework1/Program.cs
@@ -0,0 +1,29 @@
+﻿string fullName = "Корсарова Ангелина Сергеевна";
+byte age = 22;
+string email = "corsarova2001@yandex.ru";
+var pointsProgramming = 85.5;
+var pointsMath = 73.2;
+var pointsPhysics = 90.3;
+
+string pattern = $"Ф.И.О: {fullName} \nВозраст: {age} \nemail: {email} \nБаллы по программированию: {pointsProgramming} \nБаллы по математике: {pointsMath} \nБаллы по физике: {pointsPhysics}";
+
+
+Console.WriteLine(pattern,
+    fullName,
+    age,
+    email,
+    pointsProgramming,
+    pointsMath,
+    pointsPhysics);
+Console.ReadKey();
+
+double allPoints = pointsProgramming + pointsMath + pointsPhysics;
+double averagePoint = allPoints / 3;
+
+string pattern2 = $"Сумма баллов: {allPoints} \nСредний балл: {averagePoint}";
+
+Console.WriteLine(pattern2,
+    allPoints,
+    averagePoint);
+
+Console.ReadKey();
