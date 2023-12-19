8815d9d9d8dd9f302d9bd46765f9f6c8ace1ee3a
SkrinGod <Rooopppi1@gmail.com>
2023-12-19 18:17:13 +0300

    Дз4 (семенар-4)

---
+++ Homework3/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Введите предложение: ");
+string sentence = Console.ReadLine()!;
+
+var words = SplitString(sentence);
+WriteWordsToConsole(words);
+Reverse(words);
+foreach (var word in words)
+    Console.Write(word + " ");
+Console.WriteLine();
+static void WriteWordsToConsole(string[] words)
+{
+    foreach (var word in words)
+        Console.WriteLine(word);
+}
+
+static string[] SplitString(string sentence) =>
+    sentence.Split(' ');
+
+static void Reverse(string[] words)
+{
+    for (int i = 0, j = words.Length - 1; i < words.Length / 2; i++, j--)
+    {
+        var word = words[i];
+        words[i] = words[j];
+        words[j] = word;
+    }
+}
